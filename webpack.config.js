828aaaf661f84a533e81e11786a36941f366439d
MrChen2016-08-27 <1909916440@qq.com>
2017-05-05 19:57:33 +0800

    增加转换为es5
    /src/index.js 之前是es6语法，一般用户在使用时设置了babel 没有转换modules
    所以在这里设置了./src/entry/index.js 入口文件 为源文件 ./src/index.js 为转换后的出口文件

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require("webpack");
+module.exports = {
+	entry: './src/entry/index.js',
+	output: {
+		filename: './src/index.js'
+	},
+	module: {
+		loaders: [
+			{ test: /.vue$/,loader: 'vue-loader' },
+		    {
+		      test: /\.js$/,
+		      loader: 'babel-loader',
+		      exclude: /node_modules/
+		    }
+		]
+
+	},
+	resolve: {
+		alias: { 'vue$': 'vue/dist/vue.common.js' }
+	},
+	plugins: [
+		new webpack.LoaderOptionsPlugin({
+			options: {
+				babel: {
+					presets: ['es2015','stage-3']
+				}
+			}
+		})
+	]
+
+
+}
